9971b861339937ec5037c20924008fd1dd1d98e7
reza hajitabar <rezahajitabarchehre1996@gmail.com>
2025-02-14 17:09:37 -0800

    seven  commit

--- js/script.js
+++ js/script.js
@@ -1,18 +1,50 @@
-import { getCookie } from "../utils/cookie.js";
-import { getData } from "../utils/httpRequest.js";
-const loginButton=document.getElementById("login");
-const dashboardButton=document.getElementById("dashboard");
-
-const init=async()=>{
-    const cookie=getCookie();
-    if(cookie){
-        loginButton.style.display="none";
-    }else{
-        dashboardButton.style.display="none";
+import {
+    getCookie
+} from "../utils/cookie.js";
+import {
+    getData
+} from "../utils/httpRequest.js";
+const loginButton = document.getElementById("login");
+const dashboardButton = document.getElementById("dashboard");
+const mainContent = document.getElementById("products");
+const showProducts = (products) => {
+    mainContent.innerHTML = "";
+    products.forEach((product) => {
+        const jsx = `
+        <div>
+        <img alt=${product.title} src="${product.image}"/>
+        <h4>${product.title}</h4>
+        <div id="price">
+        <p>${product.price}</p>
+        <button>
+        Buy
+        <i class="fa-solid fa-cart-shopping"></i>
+        </button>
+        </div>
+        <div class="rate">
+        <i class="fa-solid fa-star"></i>
+        <span>${product.rating.rate}</span>
+        </div>
+        <div class="count">
+        <i class="fa-solid fa-user"></i>
+        <span>${product.rating.count}</span>
+        </div>
+        </div>
+        `
+        mainContent.innerHTML+=jsx
+    });
+ 
+}
+const init = async () => {
+    const cookie = getCookie();
+    if (cookie) {
+        loginButton.style.display = "none";
+    } else {
+        dashboardButton.style.display = "none";
     }
-    const allproduct=await getData("products");
-    console.log(allproduct);
-    
+    const allproduct = await getData("products");
+    showProducts(allproduct)
+
 }
 
-document.addEventListener("DOMContentLoaded",init);
+document.addEventListener("DOMContentLoaded", init);
